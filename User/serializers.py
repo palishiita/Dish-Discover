087153c146809c7a3a4aad606e10164dc1fe1083
Zuzia780 <mikusia2001@gmail.com>
2024-01-21 21:56:30 +0100

    removed unnecessary serializers

--- User/serializers.py
+++ User/serializers.py
@@ -6,12 +6,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = DishDiscoverUser
         fields = '__all__'
-
-class SavedRecipeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SavedRecipe
-        fields = '__all__'
-
-class LikedRecipeSerializer(serializers.ModelSerializer):
-    model= LikedRecipe
-    fields = '__all__'
