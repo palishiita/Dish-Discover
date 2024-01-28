ac7f46db8a0dc2cc9981c5caf436569018e56ace
Zuzia780 <mikusia2001@gmail.com>
2024-01-28 15:24:14 +0100

    small steps into token

--- authorisation/urls.py
+++ authorisation/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from authorisation.views import *
 
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('register', registration_view, name="register")
+    path('register', registration_view, name="register"),
+    path('login', obtain_auth_token, name = "login")
 ]
